7087f7b10af14e34db3babe1140a189962b356d2
Mahmood Choopani <choopani.m81@gmail.com>
2022-08-03 09:54:33 +0430

    add unit model

--- selectunit/models.py
+++ selectunit/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Unit(models.Model):
+    college = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    group_number = models.IntegerField()
+    professor_name = models.CharField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    first_day = models.CharField(choices=(
+        ('1', 'شنبه'),
+        ('2', 'یکشنبه'),
+        ('3', 'دوشنبه'),
+        ('4', 'سه‌شنبه'),
+        ('5', 'چهارشنبه'),
+        ('6', 'پنج‌شنبه'),
+        ('7', 'جمعه'),
+    ), max_length=1)
+    second_day = models.CharField(choices=(
+        ('1', 'شنبه'),
+        ('2', 'یکشنبه'),
+        ('3', 'دوشنبه'),
+        ('4', 'سه‌شنبه'),
+        ('5', 'چهارشنبه'),
+        ('6', 'پنج‌شنبه'),
+        ('7', 'جمعه'),
+    ), max_length=1)
