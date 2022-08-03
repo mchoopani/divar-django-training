a1fc151275039a7af76e040bb51d50e61b509fe3
Mahmood Choopani <choopani.m81@gmail.com>
2022-08-03 09:57:34 +0430

    add UnitForm

--- selectunit/forms.py
+++ selectunit/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from .models import Unit
 
 
 class CallWithMe(forms.Form):
     title = forms.CharField(label='عنوان', max_length=100, required=True)
-    text = forms.CharField(label='متن',max_length=250, min_length=10, required=True)
-    email = forms.EmailField(label='ایمیل',required=True)
+    text = forms.CharField(label='متن', max_length=250, min_length=10, required=True)
+    email = forms.EmailField(label='ایمیل', required=True)
 
+
+class UnitForm(forms.ModelForm):
+    class Meta:
+        model = Unit
+        fields = '__all__'
