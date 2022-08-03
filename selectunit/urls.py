df8b68d8302af2d557b663ce6cbe24ca3b0fe35a
Mahmood Choopani <choopani.m81@gmail.com>
2022-08-03 09:47:59 +0430

    add panel link to navbar

--- selectunit/urls.py
+++ selectunit/urls.py
@@ -5,4 +5,5 @@ from .views import profile, edit_profile
 urlpatterns = [
     path('profile/', profile, name='profile'),
     path('profile/edit', edit_profile, name='edit_profile'),
+    path('dashboard/', edit_profile, name='panel'), # TODO
 ]
